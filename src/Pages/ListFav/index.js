f04fa5497868ffb004667b3c9a3d6bc6ac6ead96
gisesonia <gisesonia@gmail.com>
2020-04-19 22:40:25 -0300

    Removendo dos favoritos

--- src/Pages/ListFav/index.js
+++ src/Pages/ListFav/index.js
@@ -2,20 +2,34 @@ import React, {useState, useEffect} from 'react';
 
 export default function ListFav() {
   const [repositories, setRepositories] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('https://api.github.com/users/gisesonia/repos');
+      const response = await fetch(
+        'https://api.github.com/users/gisesonia/repos'
+      );
       const data = await response.json();
-
+       //Não sei se ficaria certo acrescentar um favorite true aqui
       setRepositories(data);
     }
     fetchData();
   }, []);
 
+  //Colocaria um render condicional se false esconde o favorito, está nos requisitos
+  function handleFavorite(id) {
+      const newrepositories = repositories.map(repo =>{
+          return repo.id === id ? {...repo, favorite: false}
+      });
+      setRepositories(newRepositories);
+  }
+
   return (
     <ul>
       {repositories.map((repo) => (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+          {repo.name}
+          <button onClick={() => handleFavorite(repo.id)}></button>
+        </li>
       ))}
     </ul>
   );
