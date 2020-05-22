562564bce67eb1ce8ee04455160f890a8b767cc7
Dev4ster <victormenezes35@hotmail.com>
2020-05-22 15:44:01 -0300

    ajust in favorites funciton

--- src/Pages/StarRed/index.js
+++ src/Pages/StarRed/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {FlatList} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import {FlatList, ActivityIndicator, View} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
+import EventEmmiter from '../../events';
 
 import AppContainer from '../../Components/AppContainer';
 import Header from '../../Components/Header';
@@ -11,30 +12,30 @@ import SearchInput from '../../Components/SearchInput';
 import ListContainer from '../../Components/ListContainer';
 import ListItemsContainer from '../../Components/ListItemsContainer';
 import DevListItem from '../../Components/DevListItem';
+import useFavorites from '../../Services/useFavorites';
 
 export default function StarRed({navigation}) {
-  const [favs, setFavs] = useState();
-  const [allFavs, setAllFavs] = useState([]);
+  // const [favs, setFavs] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const [favs, favorite, reload] = useFavorites();
+
+  const focus = useIsFocused();
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      const getFavs = async () => {
-        const data = await AsyncStorage.getItem('favourites');
-        setAllFavs(JSON.parse(data));
-        setFavs(JSON.parse(data));
-      };
+    if (focus) {
+      setTimeout(() => {
+        reload(() => setLoading(false));
+      }, 2000);
+    } else {
+      setLoading(true);
+    }
+  }, [focus, reload]);
+  // const handleSearch = (search) => {
+  //   const finded = allFavs.filter((fav) => fav.login.indexOf(search) !== -1);
 
-      getFavs();
-    });
-
-    return unsubscribe;
-  }, [navigation]);
-
-  const handleSearch = (search) => {
-    const finded = allFavs.filter((fav) => fav.login.indexOf(search) !== -1);
-
-    setFavs(finded);
-  };
+  //   setFavs(finded);
+  // };
 
   return (
     <AppContainer>
@@ -52,21 +53,32 @@ export default function StarRed({navigation}) {
         />
         <ListContainer>
           <ListItemsContainer>
-            <FlatList
-              keyExtractor={(item) => String(item.id)}
-              data={favs}
-              renderItem={({item}) => (
-                <DevListItem
-                  profile={item}
-                  favorited
-                  onPress={() =>
-                    navigation.navigate('DetailDev', {
-                      profile: item,
-                    })
-                  }
-                />
-              )}
-            />
+            {loading ? (
+              <ActivityIndicator size="large" color="#5A54FF" />
+            ) : (
+              <FlatList
+                keyExtractor={(item) => String(item.id)}
+                data={favs}
+                showsVerticalScrollIndicator={false}
+                renderItem={({item}) => (
+                  <DevListItem
+                    profile={item}
+                    favorited
+                    onFavorite={favorite}
+                    onPress={() =>
+                      navigation.navigate('DetailDev', {
+                        profile: item,
+                      })
+                    }
+                  />
+                )}
+                ListFooterComponent={() => <View style={{padding: 100}} />}
+                onScrollBeginDrag={() =>
+                  EventEmmiter.dispatch('MenuOpacity', 0.3)
+                }
+                onScrollEndDrag={() => EventEmmiter.dispatch('MenuOpacity', 1)}
+              />
+            )}
           </ListItemsContainer>
         </ListContainer>
       </Container>
